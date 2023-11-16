6fd5ff2cca77d09c3f78ba1353f8864fa9a1c21b
Neil McGuire <12nmcguire@gmail.com>
2023-11-16 18:42:06 +0700

    qr grid

--- app/qrcode.tsx
+++ app/qrcode.tsx
@@ -1,15 +1,17 @@
-import "../public/kbank.jpg";
+import "../public/qrcode_kbank.jpg";
+import "../public/qrcode_line.jpeg";
 import { useEffect, useState } from "react";
 import axios from "axios";
+import Image from "next/image";
 
 export default function Qrcode() {
-  const images = ["/kbank.jpg"];
+  const images = ["/qrcode_kbank.jpg", "/qrcode_line.jpeg"];
   return (
     <>
       {images.map((image, index) => {
         return (
           <div
-            className="container flex flex-col justify-center items-center h-screen w-full bg-grey_col-100  "
+            className="container flex flex-row justify-center items-center h-screen bg-grey_col-100  "
             key={`qrcode${index}`}
           >
             <section className="container  rounded-lg flex flex-col justify-center items-center mx-7 my-[85px] bg-white w-[320px] h-[497px]  lg:w-[320px] lg:h-[497px] lg:my-[151px] lg:mx-[560px]  ">
